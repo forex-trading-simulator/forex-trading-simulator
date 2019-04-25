ef940f0df5989d719d5f1b37e50bd0ef6a0c32fd
nvtrakhmawati <rakhmawati.novita@gmail.com>
2019-04-25 13:58:51 +0700

    done login

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,11 +1,34 @@
 const routes = require('express').Router()
+const Model = require('../models')
+const userModel = Model.Users
+// const currencyModel = Model.Currencies
+// const transactionModel = Model.Transactions
 
 
 routes.get('/',(req,res) => {
     res.render('login.ejs')
 })
 
-routes.post('/login/',(req,res) => {
+routes.post('/',(req,res) => {
+    let input = {
+        username: req.body.username,
+        password: req.body.password
+    }
+    userModel.findAll()
+    .then(users => {
+        if(users.username == input.username){
+            if(users.password == input.password){
+                res.redirect('/profile')
+            }else{
+                console.log('salah password')
+            }
+        }else{
+            console.log('username salah')
+        }   
+    })
+    .catch(err => {
+        res.send(err)
+    })
     res.send(req.body)
 })
 
